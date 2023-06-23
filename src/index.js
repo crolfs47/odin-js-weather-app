3c24b5d62b2f3cd94ac36d0b8beb0044142a7dd9
Cathy Rolfs <crolfs47@gmail.com>
2023-06-23 15:09:11 -0500

    Reorg code into separate modules

--- src/index.js
+++ src/index.js
@@ -1,28 +1,5 @@
 import './style.css';
+import { getWeather } from './weatherData';
 
-const getCityInput = () => {
-  // e.preventDefault();
-  const cityInput = 'London';
-  return cityInput;
-};
-
-const getCurrentWeatherAPIData = async () => {
-  const city = getCityInput();
-  const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=5cad8c98b3024bbaaf0190940232106&q=${city}`, { mode: 'cors' });
-  const weatherData = await response.json();
-  return weatherData;
-};
-
-const getWeather = async () => {
-  const weatherData = await getCurrentWeatherAPIData();
-  const weather = {
-    city: weatherData.location.name,
-    tempC: weatherData.current.temp_c,
-    tempF: weatherData.current.temp_f,
-    condition: weatherData.current.condition.text,
-  };
-  console.log(weather.tempF);
-  return weather;
-};
 
 getWeather();
