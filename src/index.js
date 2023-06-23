fe3bc9b5a5ff6b6f9f253a9b99580cc2aa7231b7
Cathy Rolfs <crolfs47@gmail.com>
2023-06-23 13:38:45 -0500

    Add basic functions to return weather data from Weather API

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './style.css';
 
 console.log('Test');
+
+const getWeather = async (city) => {
+  const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=5cad8c98b3024bbaaf0190940232106&q=${city}`, { mode: 'cors' });
+  const weatherData = await response.json();
+  console.log(weatherData);
+};
+
+const getCityInput = () => {
+  // e.preventDefault();
+  const cityInput = 'London';
+  getWeather(cityInput);
+};
+
+getCityInput();
