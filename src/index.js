5c68f7f0afa90af0ab136e173eaf226e584722d3
Cathy Rolfs <crolfs47@gmail.com>
2023-06-23 14:38:44 -0500

    Add function to process API data and return weather object with data for app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import './style.css';
 
-console.log('Test');
-
-const getWeather = async (city) => {
-  const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=5cad8c98b3024bbaaf0190940232106&q=${city}`, { mode: 'cors' });
-  const weatherData = await response.json();
-  console.log(weatherData);
-};
-
 const getCityInput = () => {
   // e.preventDefault();
   const cityInput = 'London';
-  getWeather(cityInput);
+  return cityInput;
 };
 
-getCityInput();
+const getCurrentWeatherAPIData = async () => {
+  const city = getCityInput();
+  const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=5cad8c98b3024bbaaf0190940232106&q=${city}`, { mode: 'cors' });
+  const weatherData = await response.json();
+  return weatherData;
+};
+
+const getWeather = async () => {
+  const weatherData = await getCurrentWeatherAPIData();
+  const weather = {
+    city: weatherData.location.name,
+    tempC: weatherData.current.temp_c,
+    tempF: weatherData.current.temp_f,
+    condition: weatherData.current.condition.text,
+  };
+  console.log(weather.tempF);
+  return weather;
+};
+
+getWeather();
