0d392163886b2ecb0ddcb8a151a523ad14f66ad8
Cathy Rolfs <crolfs47@gmail.com>
2023-06-26 11:09:53 -0500

    get weather data functions working with search form input

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import './style.css';
 import { getWeather } from './weatherData';
 
-
-getWeather();
+// getWeather();
--- src/weatherData.js
+++ src/weatherData.js
@@ -1,26 +1,38 @@
+const searchForm = document.getElementById('search-form');
+
 const getCityInput = () => {
-  // e.preventDefault();
-  const cityInput = 'London';
+  const cityInput = document.getElementById('city-input').value;
   return cityInput;
 };
 
 const getCurrentWeatherAPIData = async () => {
-  const city = getCityInput();
-  const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=5cad8c98b3024bbaaf0190940232106&q=${city}`, { mode: 'cors' });
-  const weatherData = await response.json();
-  return weatherData;
+  try {
+    const city = getCityInput();
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=5cad8c98b3024bbaaf0190940232106&q=${city}`, { mode: 'cors' });
+    const weatherData = await response.json();
+    return weatherData;
+  } catch (error) {
+    console.log(`error: ${error}`);
+  }
 };
 
-const getWeather = async () => {
-  const weatherData = await getCurrentWeatherAPIData();
-  const weather = {
-    city: weatherData.location.name,
-    tempC: weatherData.current.temp_c,
-    tempF: weatherData.current.temp_f,
-    condition: weatherData.current.condition.text,
-  };
-  console.log(weather.tempF);
-  return weather;
+const getWeather = async (e) => {
+  e.preventDefault();
+  try {
+    const weatherData = await getCurrentWeatherAPIData();
+    const weather = {
+      city: weatherData.location.name,
+      tempC: weatherData.current.temp_c,
+      tempF: weatherData.current.temp_f,
+      condition: weatherData.current.condition.text,
+    };
+    console.log(weather);
+    return weather;
+  } catch (error) {
+    console.log(`error: ${error}`);
+  }
 };
+
+searchForm.addEventListener('submit', getWeather);
 
 export { getWeather };
